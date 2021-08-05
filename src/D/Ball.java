1aba4fecf9dbffb01cca963012f277e3c19fffba
=sakchaisrim <=sakchaisrim@gmail.com>
2021-08-05 10:42:32 +0700

    first commit

---
+++ src/D/Ball.java
@@ -0,0 +1,13 @@
+package D;
+
+public class Ball {
+    String color;
+    String name;
+
+    public void deflate() {
+        System.out.println("Deflate...");
+    }
+    public void inflate(){
+        System.out.println("Inflate...");
+    }
+}
